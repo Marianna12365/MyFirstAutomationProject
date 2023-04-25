0d3f446b52fc1cae7e1c1804b6e16977eb142ef4
Marianna <marianna_r@purlin.tech>
2023-04-25 11:31:16 +0400

    second commit

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,11 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int age = 20;
-if (age > 18)
+//Console.WriteLine("Hello, World!");
+//int age = 20;
+/*if (age > 18)
 {
     Console.WriteLine("Eligable for voting");
 }
 else 
   { 
     Console.WriteLine("Eligable for voting"); 
-  }
+  }*/
+int day = 1;
+
+switch (day)
+{ 
+case 1:Console.WriteLine("Sunday");break;
+case 2:Console.WriteLine("Monday"); break;
+    case 3: Console.WriteLine("Tuesday"); break;
+    case 4: Console.WriteLine("Wednesday"); break;
+    case 5: Console.WriteLine("Thursday"); break;
+    case 6: Console.WriteLine("Friday"); break;
+    case 7: Console.WriteLine("Saturday"); break;
+    default: Console.WriteLine("Invalid week days"); break;
+}
+
+
