5c96dd019800ef627aa944de11ac0efcc85f2816
Marianna <marianna_r@purlin.tech>
2023-04-25 11:09:24 +0400

    first commit

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int age = 20;
+if (age > 18)
+{
+    Console.WriteLine("Eligable for voting");
+}
+else 
+  { 
+    Console.WriteLine("Eligable for voting"); 
+  }
